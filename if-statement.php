8c81e58a8ef8a7eebc58f1391e2786fff391c09a
Manish kumar <manish5854758@gmail.com>
2021-08-28 23:45:56 +0530

    Add files via upload

---
+++ if-statement.php
@@ -0,0 +1,11 @@
+<?php
+$a = 10;
+$b = 10;
+//Method 1
+if ($a < $b) {
+    echo "<h2>A is samller</h2>";
+}
+//Method 2
+if ($a == $b) :
+    echo " A is Smaller";
+endif;
---
+++ logical-operater.php
@@ -0,0 +1,6 @@
+<?php
+$age = 20;
+// $b=20;
+if ($age >= 18 && $age <= 21) {
+    echo "You are eligible.";
+}
